afed16ea2483d3010390b327b7d78cdcdc634c77
esra akbulat <esra@klinikya.com>
2023-02-06 22:33:17 +0300

    video size change

--- src/components/track/index.jsx
+++ src/components/track/index.jsx
@@ -3,14 +3,28 @@ import styles from './styles.module.scss';
 
 const Track = (props) => {
   const ref = useRef();
+  const handleResize = (e) => {
+    document
+      .getElementsByClassName(`${styles.max}`)
+      .classList?.remove(`${styles.max}`);
+    e.target.classList.add(`${styles.max}`);
+    e.target.classList.remove(`${styles.min}`);
+  };
   useEffect(() => {
     if (props.track !== null) {
       const child = props.track.attach();
       ref.current.classList.add(props.track.kind);
       ref.current.appendChild(child);
+      child.classList.add(`${styles.min}`);
     }
   }, [props.track]);
-  return <div className={styles.track} ref={ref}></div>;
+  return (
+    <div
+      className={styles.track}
+      ref={ref}
+      onClick={(e) => handleResize(e)}
+    ></div>
+  );
 };
 
 export default Track;
